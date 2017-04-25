db770b59e2b04cbf0802d97bed565e05d8ec6e91
Arthur Stepovoy <arturka2014@yandex.ru>
2017-04-25 18:32:19 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
-	$DB_DSN_GLOBAL = "mysql:host=127.0.0.1:8889";
-	$DB_DSN = 'mysql:dbname=camagru;host=127.0.0.1:8889';
+	$DB_DSN_GLOBAL = "mysql:host=127.0.0.1:3306";
+	$DB_DSN = 'mysql:dbname=camagru;host=127.0.0.1:3306';
 	$DB_USER = 'root';
-	$DB_PASSWORD = 'root';
+	$DB_PASSWORD = '111111'; // change it with your root password (for localhost)
 ?>
